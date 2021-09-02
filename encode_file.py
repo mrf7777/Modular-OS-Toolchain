c3b79eacd541d64c40c1aee7b0289f50e8f1ae8d
mrf7777 <83775891+mrf7777@users.noreply.github.com>
2021-09-01 19:02:20 -0500

    Update encode_file.py
    Fixed encoding.
    Provided documentation in the script.

--- encode_file.py
+++ encode_file.py
@@ -1,9 +1,21 @@
 import sys
 
+"""
+"Modular OS" encoding is an invertible transformation on text.
+It is defined as the following executed in this order:
+    -all occurrences of backslash are replaced with a double backslash
+    -all occurrences of " are replaced with a backslash quote
+    -all occurrences of , are replaced with a backslash comma
+
+To undo to transformation, undo each of the steps in reverse order.
+
+This encoding ensures that text files can be uploaded to a Scratch 3.0 list.
+Commas and quote characters provide unexpected behavior upon importing.
+"""
 
 def encode_string(string):
-    encoded_string = string.replace("\"", "\\q")    # replace quotes with backslash q
-    encoded_string = encoded_string.replace("\\", "\\\\")   # replace backslash with double backslash
+    encoded_string = string.replace("\\", "\\\\")  # replace backslash with double backslash
+    encoded_string = encoded_string.replace("\"", "\\q")    # replace quotes with backslash q
     encoded_string = encoded_string.replace(",", "\\c")      # replace comma with backslash comma
     return encoded_string
 
