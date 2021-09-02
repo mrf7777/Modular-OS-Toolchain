130f3507506dc6740d6fff3ac6226a6685e3c060
mrf7777 <83775891+mrf7777@users.noreply.github.com>
2021-09-02 11:43:20 -0500

    Update mos-main.py
    Changed main.py to mos-main.py.
    This allows the script to be safely put into some PATH variable and allows access to the entire toolchain anywhere.

---
+++ mos-main.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+
+def print_usage_message():
+    print("")
+    print("Usage:")
+    print("    python.exe mos-main.py <command> <options>")
+    print("Commands (substitute <command>):")
+    print("    compile")
+    print("    encode")
+    print("    help")
+    print("Description:")
+    print("    This is a multi-tool that allows users to do anything related to Modular OS.")
+    print("    For how to use a specific command, type \"python.exe mos-main.py help <command>\".")
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    main(sys.argv)
